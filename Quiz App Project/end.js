bb57db6d39690ef5473b203e4304793904f6e1b9
Musteqeem <soladoyemustekeem@gmail.com>
2022-07-27 20:03:02 +0100

    Initial commit

---
+++ Quiz App Project/end.js
@@ -0,0 +1,33 @@
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const username = document.getElementById('username');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highscore')) || [];
+console.log(highScores);
+
+username.addEventListener('keyup', ()=> {
+    saveScoreBtn.disabled = !username.value;
+});
+
+finalScore.innerText = mostRecentScore;
+
+
+saveHighScore = e => {
+    console.log('this button has been clicked');
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value 
+    }
+     
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score)
+
+    localStorage.setItem('highscore', JSON.stringify(highScores))
+    highScores.splice(5)
+    window.location.assign('/')
+    console.log(highScores);
+}
+
